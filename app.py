a05666a36cdfe89d88e7eb68be77c7342f154b0d
Keith Kiser <kakiser56@me.com>
2023-09-05 11:29:19 -0400

    add beginning stuff

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
+import openai
 from flask import Flask, render_template, request
+from dotenv import dotenv_values
+import json
+
+config = dotenv_values('.env');
+openai.api_key = config["OPENAI_API_KEY"]
+
 app = Flask(__name__,
             template_folder='templates')
 @app.route("/")
