1b871edb8ac230d43e8e764d6868f485f56506b5
Keith Kiser <kakiser56@me.com>
2023-09-07 13:52:41 -0400

    minor updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ColorPalette
+Class link https://www.udemy.com/course/mastering-openai/learn/lecture/37132458#overview
--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__,
             template_folder='templates')
 @app.route("/")
 def index():
-    return "Hello from flask!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
